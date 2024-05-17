937f605dfc147dd5ae5010619f67c80d558e639f
Aleksandr Krasavin <krasa19920906@gmail.com>
2024-05-17 22:52:53 +0300

    add tasks server,clean,img

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -2,13 +2,23 @@ import gulp from "gulp";
 import fileInclude from "gulp-file-include";
 import nodeSass from 'sass';
 import gulpSass from 'gulp-sass';
+import server from "gulp-server-livereload";
+import clean from "gulp-clean";
+import fs from "fs";
 const sass = gulpSass(nodeSass);
-
 const {src,dest} = gulp;
 
+// Таска для очищения dist
+gulp.task("clean", function (done) {
+    if(fs.existsSync("./dist/")) {
+        return src("./dist/", {read:false})
+            .pipe(clean({force:true}))
+    };
+    done();
+})
 
 // Таска для добавления Html шаблонов
-gulp.task("includeFiles", function () {
+gulp.task("html", function () {
     return src("./src/*.html")
         .pipe(fileInclude ({
             prefix: "@@",
@@ -22,4 +32,17 @@ gulp.task("sass",function () {
     return src("./src/scss/*.scss")
         .pipe(sass())
         .pipe(dest("./dist/css/"))
+})
+
+// Копирование картинок
+gulp.task("img", function() {
+    return src("./src/img/**/*").pipe("./dist/img/")
+})
+
+// Автообновление сервера
+gulp.task("server", function() {
+    return src("./dist").pipe(server({
+        livereload:true,
+        open:true
+    }))
 })
