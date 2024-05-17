8d55ec7a7966528a9818d37907e382d9458b6c5d
Aleksandr Krasavin <krasa19920906@gmail.com>
2024-05-17 23:53:35 +0300

    add def task,watch,fonts

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -5,6 +5,9 @@ import gulpSass from 'gulp-sass';
 import server from "gulp-server-livereload";
 import clean from "gulp-clean";
 import fs from "fs";
+import map from "gulp-sourcemaps";
+import plumber from "gulp-plumber";
+import notify from "gulp-notify";
 const sass = gulpSass(nodeSass);
 const {src,dest} = gulp;
 
@@ -20,6 +23,14 @@ gulp.task("clean", function (done) {
 // Таска для добавления Html шаблонов
 gulp.task("html", function () {
     return src("./src/*.html")
+        .pipe(plumber({
+            errorHandler: notify.onError ({
+                title:"html",
+                message: "Error <%= error.message%>",
+                sound:false
+            })
+        }
+        ))
         .pipe(fileInclude ({
             prefix: "@@",
             basepath: "@file"
@@ -30,13 +41,28 @@ gulp.task("html", function () {
 // Таска для компиляции scss
 gulp.task("sass",function () {
     return src("./src/scss/*.scss")
+        .pipe(plumber({
+            errorHandler: notify.onError ({
+                title:"style",
+                message: "Error <%= error.message%>",
+                sound:false
+            })
+        }
+        ))
+        .pipe(map.init())
         .pipe(sass())
+        .pipe(map.write())
         .pipe(dest("./dist/css/"))
 })
 
 // Копирование картинок
 gulp.task("img", function() {
-    return src("./src/img/**/*").pipe("./dist/img/")
+    return src("./src/img/**/*").pipe(dest("./dist/img/"))
+})
+
+// Копирование Шрифтов
+gulp.task("fonts", function() {
+    return src("./src/fonts/**/*").pipe(dest("./dist/fonts/"))
 })
 
 // Автообновление сервера
@@ -46,3 +72,18 @@ gulp.task("server", function() {
         open:true
     }))
 })
+
+// Слежение за файлами
+gulp.task("watch", function () {
+    gulp.watch("./src/scss/**/*.scss", gulp.parallel("sass"))
+    gulp.watch("./src/img/**/*", gulp.parallel("img"))
+    gulp.watch("./src/fonts/**/*", gulp.parallel("fonts"))
+    gulp.watch("./src/**/*.html", gulp.parallel("html"))
+
+})
+
+gulp.task ("default", gulp.series(
+    "clean",
+    gulp.parallel("html","sass","img","fonts"),
+    gulp.parallel("watch","server"),
+))
